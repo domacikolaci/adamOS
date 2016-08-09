054671b156f54146dd0daff5fef0a67177738193
Adam Knezevic <adam_knezi@outlook.com>
2016-08-09 22:06:09 +1000

    Final changes for the nigh
    Has changes such as:
    -Me giving up on the game for now
    -Updating the commenting
    -Posibly other random features! (I honestly dont know what ive changed
    now :/ Not much though)

--- adamOS/adventureGame/adventureGameMain.cs
+++ adamOS/adventureGame/adventureGameMain.cs
@@ -6,11 +6,22 @@ using System.Threading.Tasks;
 
 namespace adamOS.adventureGame
 {
+    
     public class adventureGameMain
     {
+        public static string playerName;
+
         public static void gameMain()
         {
-            Console.Write("lmao, game class xd");
+            Console.Write("Welcome adventurer! Please, state thy name!");
+            var playerName = Console.ReadLine();
+            testWew();
+        }
+
+        //Shits breaking here.... Will investigate later
+        public static void testWew()
+        {
+            Console.Write(playerName);
             Console.ReadLine();
         }
     }
