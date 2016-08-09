31d9721f9402d52f1723b26a818852cbe4fb5aba
Adam Knezevic <adam_knezi@outlook.com>
2016-08-09 17:09:47 +1000

    Added a TODO and that

--- adamOS/Kernel.cs
+++ adamOS/Kernel.cs
@@ -2,22 +2,43 @@
 using System.Collections.Generic;
 using System.Text;
 using Sys = Cosmos.System;
-
 namespace adamOS
 {
+    /// <summary>
+    /// This project is based off Cosmos, and is written by Adam "domacikolaci" Knezevic
+    /// 
+    /// TODO:
+    /// IMPORTANT
+    /// -Begin basic commands
+    /// --Help
+    /// -Begining code of basic game
+    /// --Text adventure game
+    /// </summary>
     public class Kernel : Sys.Kernel
     {
         protected override void BeforeRun()
         {
-            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+            Console.WriteLine("adamOS booted successfully. Please enter a command bellow.");
+            Console.WriteLine("If your not sure what to type, type help for a list of commands!");
         }
 
         protected override void Run()
         {
             Console.Write("Input: ");
             var input = Console.ReadLine();
-            Console.Write("Text typed: ");
-            Console.WriteLine(input);
+            //Checks if the help command has been run
+            if (input == "help")
+            {
+                help();
+            }
+            
+        }
+
+        //This is the "help" command
+        void help()
+        {
+            Console.Write("lmao");
+            Console.ReadLine();
         }
     }
 }
