1fc902830bf6e55af784cca1adc0c6b452fe5163
Adam Knezevic <adam_knezi@outlook.com>
2016-08-22 18:46:24 +1000

    Upgraded Cosmos to DevKit

---
+++ adamOS/adventureGameMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adamOS.adventureGame
+{
+    
+    public class adventureGameMain
+    {   
+        //public static string playerName = "";
+
+        public static void gameMain()
+        {
+            //string playerName = player.playerName();
+
+            Console.Write("Welcome adventurer! Please, state thy name!\n");
+           // playerName = Console.ReadLine();
+           // Console.Write(playerName + "! You are about to embark on an amazing journey!/n");
+            Console.Write("It will involve all sorts of cool and fun quests!/n");
+            Console.Write("Are you ready?/n");
+            var playerReady = Console.ReadLine();
+            if (playerReady == "yes")
+            {
+                level1.main();
+            }
+            else
+            {
+                //Will go back to "main.cs" which will be added later.....
+                Console.WriteLine("lol k");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ adamOS/level1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adamOS.adventureGame
+{
+    public class level1
+    {
+        
+
+        public static void main()
+        {
+            Console.WriteLine("You are walking down the road to a local township when suddenly, you hear something move/n");
+            Console.WriteLine("Its a goblen! Kill it quick!");
+            goblenFight();
+        }
+
+        static void goblenFight()
+        {
+            
+        }
+    }
+}
