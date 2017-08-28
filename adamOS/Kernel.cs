e2855b1cedd0a699a9526d34080286a8e3b423ef
Adam Knezevic <adam_knezi@outlook.com>
2017-08-28 21:45:50 +1000

    NEW YEAR, NEW START!
    Initial changes to VS17, new code of Cosmos and a new your!

--- adamOS/Kernel.cs
+++ adamOS/Kernel.cs
@@ -1,35 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+//using adamOS.adventureGame; Dosnt work ATM but it loads the game
 using Sys = Cosmos.System;
-using adamOS.adventureGame;
-using System.IO;
 
 namespace adamOS
 {
-    /// <summary>
-    /// This project is based off Cosmos, and is written by Adam "domacikolaci" Knezevic
-    /// 
-    /// TODO:
-    /// IMPORTANT
-    /// -Begin basic commands
-    /// --Help
-    /// -Begining code of basic game
-    /// --Got to get basic fight going
-    /// --Setup basic Player stats
-    /// -Learn how to do a basic file system
-    /// --Make my own version of Vim
-    /// --Read/write to files/folders
-    /// </summary>
-    public class Kernel : Sys.Kernel
+    public class Kernel: Sys.Kernel
     {
         protected override void BeforeRun()
         {
-            //Starts the filesystem
-            var fs = new Sys.FileSystem.CosmosVFS();
-            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
-
-            //Boot message
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("adamOS booted successfully. Please enter a command bellow.");
             Console.ForegroundColor = ConsoleColor.Red;
@@ -37,12 +17,11 @@ namespace adamOS
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("If your not sure what to type, type help for a list of commands!");
         }
-
+        
         protected override void Run()
         {
             Console.Write(">: ");
             var input = Console.ReadLine();
-            //Checks if the help command has been run
             if (input == "help")
             {
                 //Loads help.cs and talks to the helpList method
@@ -52,31 +31,24 @@ namespace adamOS
             //Loads the game - Currently not working
                 if (input == "game")
             {
-                adventureGameMain.gameMain();
+                //adventureGameMain.gameMain();
             }
             else
-                if(input == "newText") //CURRENTLY BROKEN
+                if (input == "newText") //CURRENTLY BROKEN
             {
                 Console.WriteLine("Please enter some text");
                 var txt = Console.ReadLine();
                 //Console.WriteLine("Please give the file a name");
                 //var fileName = Console.ReadLine();
                 //File.WriteAllText(@"0:\" + fileName + ".txt", txt);
-                File.WriteAllText(@"0:\meme.txt", txt);
+                //File.WriteAllText(@"0:\meme.txt", txt);
             }
             else
-                if(input == "readText") //ALSO DOSNT WORK
+                if (input == "readText") //ALSO DOSNT WORK
             {
                 //var output = ReadText.ReadText();
 
-            }        
+            }
         }
-        public string ReadText(string FileAddr) //Returns the file in a single string.
-        {
-            string f_contents = "";
-            f_contents = File.ReadAllText(FileAddr);
-            return f_contents;
-        }
-
     }
 }
